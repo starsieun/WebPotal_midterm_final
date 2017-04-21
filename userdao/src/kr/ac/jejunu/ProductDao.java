7e156ee646999156eff904bf66607da9de2edef1
starsieun <risieun513@naver.com>
2017-04-22 02:15:16 +0900

    tag_8

--- userdao/src/kr/ac/jejunu/ProductDao.java
+++ userdao/src/kr/ac/jejunu/ProductDao.java
@@ -21,41 +21,86 @@ public class ProductDao {
     }
 
 
-    public Product get(Long id) throws ClassNotFoundException, SQLException {
+    public Product get(Long id) throws SQLException {
 
-        Connection connection = dataSource.getConnection();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        Product product = null;
+        try {
+            connection = dataSource.getConnection();
 
-        PreparedStatement preparedStatement = connection.prepareStatement("select * from product where id = ?");
-        preparedStatement.setLong(1, id);
+            preparedStatement = connection.prepareStatement("select * from product where id = ?");
+            preparedStatement.setLong(1, id);
 
-        ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet = preparedStatement.executeQuery();
 
-        resultSet.next();
-        Product product = new Product();
-        product.setId(resultSet.getLong("id"));
-        product.setTitle(resultSet.getString("title"));
-        product.setPrice(resultSet.getInt("price"));
+            resultSet.next();
+            product = new Product();
+            product.setId(resultSet.getLong("id"));
+            product.setTitle(resultSet.getString("title"));
+            product.setPrice(resultSet.getInt("price"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if(resultSet != null)
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if(preparedStatement != null)
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if(connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
 
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+        }
+
 
         return product;
     }
 
-    public void add(Product product) throws SQLException, ClassNotFoundException {
+    public void add(Product product) throws SQLException {
 
-        Connection connection = dataSource.getConnection();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
 
-        PreparedStatement preparedStatement = connection.prepareStatement("insert into product(id, title, price) VALUE (?, ?, ?)");
-        preparedStatement.setLong(1, product.getId());
-        preparedStatement.setString(2, product.getTitle());
-        preparedStatement.setInt(3, product.getPrice());
+            preparedStatement = connection.prepareStatement("insert into product(id, title, price) VALUE (?, ?, ?)");
+            preparedStatement.setLong(1, product.getId());
+            preparedStatement.setString(2, product.getTitle());
+            preparedStatement.setInt(3, product.getPrice());
 
-        preparedStatement.executeUpdate();
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if(preparedStatement != null)
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if(connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
 
-        preparedStatement.close();
-        connection.close();
+        }
+
 
 
 
